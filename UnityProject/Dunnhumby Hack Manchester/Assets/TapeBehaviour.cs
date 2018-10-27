12d0198e7793202b0970d2d5a8a4f26ab14801cf
Jack Scotson <jack.scotson@netacea.com>
2018-10-27 18:15:24 +0100

    adding some tape behaviour

--- UnityProject/Dunnhumby Hack Manchester/Assets/TapeBehaviour.cs
+++ UnityProject/Dunnhumby Hack Manchester/Assets/TapeBehaviour.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class TapeBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    IEnumerator Start () {
 	    Texture2D tex;
 	    tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
 	    using (WWW www = new WWW("https://www.covercentury.com/covers/dvd/d/deadpool-2016-r1-custom.jpg"))
 	    {
-	        www.LoadImageIntoTexture(tex);
+	        yield return www;
+            www.LoadImageIntoTexture(tex);
 	        GetComponent<Renderer>().material.mainTexture = tex;
 	    }
     }
