3a645c72ec07ab9ba2112f6c795ec48a7633d2e9
Andy <andystill@gmail.com>
2018-10-28 00:43:45 +0100

    Add data files

---
+++ DataProject/NFBB.Core/Shopper.cs
@@ -0,0 +1,34 @@
+﻿using NFBB.Core.Data;
+using NFBB.Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace NFBB.Core
+{
+    public class Shopper
+    {
+        private MovieRepository movieRepo;
+
+        public List<Movie> ShelfContent = new List<Movie>();
+
+        public Shopper()
+        {
+            var connectionstring = "Data Source=queue.cnucaihvfwwr.eu-west-1.rds.amazonaws.com;Initial Catalog=HackMcr2018;User ID=hm2018;Password=Random@@12345";
+
+            IDbConnection db = new SqlConnection(connectionstring);
+            movieRepo = new MovieRepository(db);
+            var urepo = new UserRepository(db);
+        }
+
+        public IEnumerable<Movie> PopulateShelves()
+        {
+            var movies = movieRepo.GetAll();
+
+            return movies;
+
+        }
+    }
+}
