2ad1f06ed546d0a8c91f362a13036abf5b4deca1
Safder Iqbal <safder.iqbal@netacea.com>
2018-10-27 23:25:38 +0100

    Attempt 1... Doesn't work

---
+++ UnityProject/Dunnhumby Hack Manchester/Assets/Safder Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Material noGaze;
+
+    public Material gaze;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material = noGaze;
+    }
+
+    public void OnViewed()
+    {
+        GetComponent<Renderer>().material = gaze;
+    }
+
+    public void NoView()
+    {
+        GetComponent<Renderer>().material = noGaze;
+    }
+}
