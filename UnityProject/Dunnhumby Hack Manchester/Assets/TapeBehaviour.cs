d4338334de8d453c67170897526683d450d48571
Jack Scotson <jack.scotson@netacea.com>
2018-10-27 17:37:49 +0100

    Starting the tape boxes and hopefully adding player

---
+++ UnityProject/Dunnhumby Hack Manchester/Assets/TapeBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapeBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Texture2D tex;
+	    tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+	    using (WWW www = new WWW("https://www.covercentury.com/covers/dvd/d/deadpool-2016-r1-custom.jpg"))
+	    {
+	        www.LoadImageIntoTexture(tex);
+	        GetComponent<Renderer>().material.mainTexture = tex;
+	    }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
