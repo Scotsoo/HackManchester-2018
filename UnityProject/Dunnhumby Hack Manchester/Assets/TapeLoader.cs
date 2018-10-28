8bc23b0e381fbe61b8851af177a864633cb7f999
Jack Scotson <jack.scotson@netacea.com>
2018-10-28 02:49:38 +0000

    Loading pics using the database

--- UnityProject/Dunnhumby Hack Manchester/Assets/TapeLoader.cs
+++ UnityProject/Dunnhumby Hack Manchester/Assets/TapeLoader.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using NFBB.Core;
+using UnityEditorInternal;
 
 public class Tape
 {
@@ -17,42 +20,48 @@ public class TapeLoader : MonoBehaviour
 
     IEnumerator Start()
     {
-        var tapes = new[]
-       {
+        Shopper s = new Shopper();
 
-            new Tape("https://m.media-amazon.com/images/M/MV5BNDg2NjIxMDUyNF5BMl5BanBnXkFtZTgwMzEzNTE1NTM@._V1_SY1000_CR0,0,629,1000_AL_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZTgwMTQ5MzM0NjM@._V1_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BYzEyYzg5N2YtZmYzZC00OTg0LWE3ZmYtNDZhMGFkOTBjOTYxXkEyXkFqcGdeQXVyNDg2MjUxNjM@._V1_SY1000_CR0,0,631,1000_AL_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BMTc5NzI1NjY4MV5BMl5BanBnXkFtZTgwNDIxNTIyNDM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BZDVkMWJiMzUtNjQyOS00MGVmLWJhYmMtN2IxYzU4MjY3MDRmXkEyXkFqcGdeQXVyNzA5NjIzODk@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BMDBhOTMxN2UtYjllYS00NWNiLWE1MzAtZjg3NmExODliMDQ0XkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_SY1000_CR0,0,631,1000_AL_.jpg"),
-            new Tape("https://m.media-amazon.com/images/M/MV5BNDg2NjIxMDUyNF5BMl5BanBnXkFtZTgwMzEzNTE1NTM@._V1_SY1000_CR0,0,629,1000_AL_.jpg")
-       };
         var rnd = new System.Random();
-        var gameTapes = GameObject.FindGameObjectsWithTag("TapeFront");
-        var len = tapes.Length - 1;
-        foreach (var tape in gameTapes)
+        
+        var tapeGroups = GameObject.FindGameObjectsWithTag("TapeGroup");
+        foreach (var tapeGroup in tapeGroups)
         {
-
-            var tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
-            var randomNumber = rnd.Next(0, len);
-            var randomTape = tapes[randomNumber];
-            if (randomTape.Texture != null)
+            var genre = tapeGroup.GetComponent<TapeGroup>().GroupGenre;
+            var movies = s.PopulateShelvesForGenre(genre).ToList();
+            var len = movies.Count;
+            var children = tapeGroup.transform.GetComponentsInChildren<TapeMetadata>();
+            foreach (var child in children)
             {
-                tape.GetComponent<Renderer>().material.mainTexture = randomTape.Texture;
-            }
-            else
-            {
-                using (var www = new WWW(randomTape.URL))
+                var randomNumber = rnd.Next(0, len);
+                var randomMovie = movies[randomNumber];
+                var tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+                using (var www = new WWW(randomMovie.PosterUrl))
                 {
                     yield return www;
                     www.LoadImageIntoTexture(tex);
-                    randomTape.Texture = tex;
-                    tapes[randomNumber] = randomTape;
-                    tape.GetComponent<Renderer>().material.mainTexture = tex;
+                    var frontCover = GetChildObject(child.gameObject.transform, "TapeFront");
+                    frontCover.GetComponent<Renderer>().material.mainTexture = tex;
                 }
             }
         }
+    }
+    public GameObject GetChildObject(Transform parent, string _tag)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            if (child.tag == _tag)
+            {
+                return child.gameObject;
+            }
+            if (child.childCount > 0)
+            {
+                GetChildObject(child, _tag);
+            }
+        }
+
+        return null;
     }
     // Update is called once per frame
     void Update()
---
+++ UnityProject/Dunnhumby Hack Manchester/Assets/TapeMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NFBB.Core.Data;
+using UnityEngine;
+
+public class TapeMetadata : MonoBehaviour
+{
+
+    public Movie MovieMetadata = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
