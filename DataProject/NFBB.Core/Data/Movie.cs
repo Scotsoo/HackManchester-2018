4bb3755d4b30ad6b50a7aab41c6c5345d82f81c6
Andy <andystill@gmail.com>
2018-10-28 01:51:48 +0100

    adding back /data folder

---
+++ DataProject/NFBB.Core/Data/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFBB.Core.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public string IMDBId { get; set; }
+
+        public string PosterUrl { get; set; }
+
+    }
+}
---
+++ DataProject/NFBB.Core/Data/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFBB.Core.Data
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Image { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
+
+
+
+
+
+    
