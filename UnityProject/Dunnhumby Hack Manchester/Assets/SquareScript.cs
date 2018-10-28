cfb7a0395cb73f2255d098c1e2f5542796f98273
Jack Scotson <jack.scotson@netacea.com>
2018-10-28 04:05:00 +0000

    Putting boxes in scene

---
+++ UnityProject/Dunnhumby Hack Manchester/Assets/SquareScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnLook ()
+    {
+        Debug.Log("LOOKING!!");
+    }
+
+    public void OnClick()
+    {
+        var movieId = gameObject.GetComponent<TapeMetadata>().MovieMetadata.Id;
+        Debug.Log("CLICKING!!");
+    }
+}
