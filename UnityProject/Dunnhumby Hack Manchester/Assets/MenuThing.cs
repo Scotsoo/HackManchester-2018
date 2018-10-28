c8f46af6a98825b81dde838b290e608b7594c0d1
Jack Scotson <jack.scotson@netacea.com>
2018-10-28 07:38:03 +0000

    adding a modal overlay to show some data

---
+++ UnityProject/Dunnhumby Hack Manchester/Assets/MenuThing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuThing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+   
+}
